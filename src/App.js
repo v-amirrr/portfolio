80434e04bd19ffcb9d2c55d70e1044af024b0eb7
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-08-02 20:53:42 +0430

    feat: built home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+import { AnimatePresence } from 'framer-motion';
+
+import Home from './components/Home';
 
 const App = () => {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        localStorage.setItem("lan", "en");
+    }, []);
+
     return (
         <>
-            
+            <AnimatePresence exitBeforeEnter>
+                <Routes location={location} key={location.key}>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </AnimatePresence>
         </>
     );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,151 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import { motion } from 'framer-motion';
+
+const homePageVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.4, type: 'tween', when: "beforeChildren", staggerChildren: 0.5, delayChildren: 0.5 } },
+    exit: { opacity: 0, transition: { duration: 0.4, type: 'tween', when: "afterChildren" } }
+};
+
+const titleVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.4, type: 'tween' } },
+    exit: { opacity: 0, y: -20, transition: { duration: 0.4, type: 'tween' } }
+};
+
+const buttonsVariants = {
+    hidden: { opacity: 0, scaleX: 0 },
+    visible: { opacity: 1, scaleX: 1, transition: { duration: 0.4, type: 'tween' } },
+    exit: { opacity: 0, scaleY: 0, transition: { duration: 0.4, type: 'tween' } }
+};
+
+const Home = () => {
+    return (
+        <>
+            <HomePage initial='hidden' animate='visible' exit='exit' variants={homePageVariants}>
+                <Background><img src='images/bg2.jpg' /></Background>
+
+                <Title variants={titleVariants}>
+                        <h1>Hi, I'm Amir.</h1>
+                        <h1>I'm a Front-End developer.</h1>
+                </Title>
+
+                <Buttons variants={buttonsVariants}>
+                    <Button whileTap={{ scale: 0.8 }}>
+                        let's go
+                    </Button>
+
+                    <Button whileTap={{ scale: 0.8 }}>
+                        resume
+                    </Button>
+                </Buttons>
+            </HomePage>
+        </>
+    );
+};
+
+const HomePage = styled(motion.section)`
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-position: top right;
+    background-blend-mode: multiply;
+    background-color: #00000099;
+`;
+
+const Background = styled(motion.div)`
+    position: absolute;
+    inset: 0 0 0 0;
+    z-index: -9;
+    
+    img {
+        filter: blur(50px);
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const Title = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 1rem;
+    
+    h1 {
+        font-family: 'Outfit', sans-serif;
+        line-height: 1.5;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        
+        &:nth-child(1) {
+            font-size: 3rem;
+            word-spacing: 10px;
+            letter-spacing: -2px;
+            color: #fff;
+        }
+        
+        &:nth-child(2) {
+            font-size: 1rem;
+            color: #f0f0f0;
+            font-weight: 500;
+        }
+    }
+`;
+
+const Buttons = styled(motion.div)`
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row-reverse;
+`;
+
+const Button = styled(motion.button)`
+    all: unset;
+    font-family: 'Outfit', sans-serif;
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    border-radius: 10px;
+    width: 8rem;
+    height: 3rem;
+    word-spacing: 5px;
+    letter-spacing: -1px;
+    white-space: nowrap;
+    cursor: pointer;
+    text-align: center;
+    margin: 0 1rem;    
+    backdrop-filter: blur(10px) saturate(150%);
+    -webkit-backdrop-filter: blur(10px) saturate(150%);
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: background-color .4s, box-shadow .4s, border .4s;
+    box-shadow: #00000011 0px 4px 12px;
+    border: solid 1px #ffffff11;
+    
+    &:hover {
+        background-color: #ffffff11;
+        border: solid 1px #ffffff19;
+        box-shadow: #00000088 0px 4px 12px;
+    }
+
+    &:first-child {
+        font-weight: 900;
+    }
+    
+    &:last-child {
+        font-weight: 300;
+    }
+`;
+
+export default Home;
